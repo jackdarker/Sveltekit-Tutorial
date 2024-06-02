b1c9ba42e103b83037b679934c409e3a4fdad5e4
Jack Darker <JackDarker@freenet.de>
2024-06-02 20:02:44 +0200

    fix .env

--- my-app/runServer.cmd
+++ my-app/runServer.cmd
@@ -1,2 +1,2 @@
 echo run the distribution from build-directory
-node -r dotenv/config build dotenv_config_path=D:/Projects/HTML/Sveltekit-Tutorial/my-app/build/.env
+node -r dotenv/config build dotenv_config_path=./build/.env
